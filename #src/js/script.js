40f2c653524d20c60b44cf66b4160ea201d15796
life-f <58433392+life-f@users.noreply.github.com>
2021-08-09 12:28:42 +0300

    header menu

--- #src/js/script.js
+++ #src/js/script.js
@@ -3,99 +3,99 @@ $(document).ready(function () {
     /**
      *  Фиксация header
      */
-    window.onscroll = function () {
-        myFunction()
-    };
-    var header = document.getElementById("header-top");
-    var sticky = 10;
-    if (window.screen.width <= '768') {
-        header = document.getElementById("header");
-        sticky = 0
-    }
-
-    function myFunction() {
-        if (window.pageYOffset >= sticky) {
-            header.classList.add("fixed");
-        } else {
-            header.classList.remove("fixed");
-        }
-    }
+    // window.onscroll = function () {
+    //     myFunction()
+    // };
+    // var header = document.getElementById("header-top");
+    // var sticky = 10;
+    // if (window.screen.width <= '768') {
+    //     header = document.getElementById("header");
+    //     sticky = 0
+    // }
+    //
+    // function myFunction() {
+    //     if (window.pageYOffset >= sticky) {
+    //         header.classList.add("fixed");
+    //     } else {
+    //         header.classList.remove("fixed");
+    //     }
+    // }
 
     /**
      * Меню на мобильных
      */
-    if ($(window).width() <= '768') {
-        $('.header__menu').addClass('sidenav')
-    }
-    $('.header__mobile').on('click', function () {
-        if ($(this).hasClass('change')) {
-            $('.header__mobile').removeClass('change')
-            $('.header__menu').removeClass('width')
-        } else {
-            $('.header__mobile').addClass('change')
-            $('.header__menu').addClass('width')
-        }
-    })
-
-    $('.menu__link').on('click', function(){
-        $('.header__mobile').removeClass('change')
-        $('.header__menu').removeClass('width')
-    })
+    // if ($(window).width() <= '768') {
+    //     $('.header__menu').addClass('sidenav')
+    // }
+    // $('.header__mobile').on('click', function () {
+    //     if ($(this).hasClass('change')) {
+    //         $('.header__mobile').removeClass('change')
+    //         $('.header__menu').removeClass('width')
+    //     } else {
+    //         $('.header__mobile').addClass('change')
+    //         $('.header__menu').addClass('width')
+    //     }
+    // })
+    //
+    // $('.menu__link').on('click', function(){
+    //     $('.header__mobile').removeClass('change')
+    //     $('.header__menu').removeClass('width')
+    // })
 
     /**
      * Определение слайдеров
      */
-    if ($(window).width() <= '768') {
-        $('.works__slider').slick({
-            variableWidth: true,
-            slidesToShow: 1,
-            arrows: false,
-            dots: true,
-            centerMode: true,
-            centerPadding: '40px'
-        });
-    } else if ($(window).width() > '768') {
-        $('.works__slider').slick({
-            variableWidth: false,
-            dots: true,
-            arrows: false,
-            infinite: false,
-            slidesToShow: 3,
-        });
-    }
-
-    if ($(window).width() <= '568') {
-        $('.reviews__slider').slick({
-            variableWidth: false,
-            arrows: true,
-            prevArrow: '<div class="arrow-prev arrow"><img src="../img/arrow-prev.png"></div>',
-            nextArrow: '<div class="arrow-next arrow"><img src="../img/arrow-next.png"></div>',
-            dots: false,
-            infinite: false,
-            slidesToShow: 1,
-        });
-    } else if ($(window).width() <= '768') {
-        $('.reviews__slider').slick({
-            variableWidth: false,
-            arrows: true,
-            prevArrow: '<div class="arrow-prev arrow"><img src="../img/arrow-prev.png"></div>',
-            nextArrow: '<div class="arrow-next arrow"><img src="../img/arrow-next.png"></div>',
-            dots: false,
-            infinite: false,
-            slidesToShow: 2,
-        });
-    }
+    // if ($(window).width() <= '768') {
+    //     $('.works__slider').slick({
+    //         variableWidth: true,
+    //         slidesToShow: 1,
+    //         arrows: false,
+    //         dots: true,
+    //         centerMode: true,
+    //         centerPadding: '40px'
+    //     });
+    // } else if ($(window).width() > '768') {
+    //     $('.works__slider').slick({
+    //         variableWidth: false,
+    //         dots: true,
+    //         arrows: false,
+    //         infinite: false,
+    //         slidesToShow: 3,
+    //     });
+    // }
+    //
+    // if ($(window).width() <= '568') {
+    //     $('.reviews__slider').slick({
+    //         variableWidth: false,
+    //         arrows: true,
+    //         prevArrow: '<div class="arrow-prev arrow"><img src="../img/arrow-prev.png"></div>',
+    //         nextArrow: '<div class="arrow-next arrow"><img src="../img/arrow-next.png"></div>',
+    //         dots: false,
+    //         infinite: false,
+    //         slidesToShow: 1,
+    //     });
+    // } else if ($(window).width() <= '768') {
+    //     $('.reviews__slider').slick({
+    //         variableWidth: false,
+    //         arrows: true,
+    //         prevArrow: '<div class="arrow-prev arrow"><img src="../img/arrow-prev.png"></div>',
+    //         nextArrow: '<div class="arrow-next arrow"><img src="../img/arrow-next.png"></div>',
+    //         dots: false,
+    //         infinite: false,
+    //         slidesToShow: 2,
+    //     });
+    // }
 
     /**
      * Форма для заявки
      */
-    $('.btn').on("click", function () {
-        $('.pop-up__card').addClass('active')
-        $('.pop-up__wall').addClass('active')
-    })
-
-    $('.pop-up__close-btn').on("click", function () {
-        $('.pop-up__card').removeClass('active')
-        $('.pop-up__wall').removeClass('active')
-    })
+    // $('.btn').on("click", function () {
+    //     $('.pop-up__card').addClass('active')
+    //     $('.pop-up__wall').addClass('active')
+    // })
+    //
+    // $('.pop-up__close-btn').on("click", function () {
+    //     $('.pop-up__card').removeClass('active')
+    //     $('.pop-up__wall').removeClass('active')
+    // })
 })
