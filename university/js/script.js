ace79e71572efd761b52ff29cf67934eef1e6e65
life-f <58433392+life-f@users.noreply.github.com>
2022-07-12 10:54:58 +0300

    add build

---
+++ university/js/script.js
@@ -0,0 +1,184 @@
+$(document).ready(function () {
+    if ($(window).width() <= '1024') {
+        $('.container-slider').removeClass('container-slider')
+    }
+
+    /**
+     * Меню на мобильных
+     */
+    if ($(window).width() <= '1024') {
+        $('.header__menu').addClass('sidenav')
+    }
+    $('.header__burger-menu').on('click', function () {
+        if ($(this).hasClass('change')) {
+            $('.header__burger-menu').removeClass('change')
+            $('.header__menu').removeClass('width')
+            $('.pop-up__wall').removeClass('active')
+        } else {
+            $('.header__burger-menu').addClass('change')
+            $('.header__menu').addClass('width')
+            $('.pop-up__wall').addClass('active')
+        }
+    })
+
+    $('.menu__link').on('click', function () {
+        $('.header__mobile').removeClass('change')
+        $('.header__menu').removeClass('width')
+    })
+
+    /**
+     * Определение слайдера преимуществ
+     */
+    if ($(window).width() <= '1024') {
+        $('.benefits__list-slider').slick({
+            slidesToShow: 1,
+            dots: true,
+            arrows: false,
+            infinite: false
+        })
+    }
+
+    /**
+     * Определение слайдера выпускников
+     */
+    $('.graduates__photos-slider').slick({
+        variableWidth: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        asNavFor: '.graduates__review-slider',
+        infinite: false
+    });
+    $('.graduates__review-slider').slick({
+        variableHeight: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.graduates__photos-slider',
+        arrows: true,
+        prevArrow: '<div class="arrow-prev arrow"></div>',
+        nextArrow: '<div class="arrow-next arrow"></div>',
+        centerMode: false,
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 1025,
+                settings: {
+                    arrows: false,
+                    dots: true
+                }
+            }
+        ]
+    });
+
+    /**
+     * Определение слайдера цен
+     */
+    if ($(window).width() > '1024') {
+        $('.price-slider').slick({
+            variableWidth: false,
+            variableHeight: false,
+            slidesToShow: 3,
+            arrows: true,
+            prevArrow: '<div class="arrow-prev arrow"></div>',
+            nextArrow: '<div class="arrow-next arrow"></div>',
+            infinite: false
+        })
+    }
+
+    /**
+     * Определение слайдера преподавателей
+     */
+    if ($(window).width() >= '768') {
+        $('.teachers-slider').slick({
+            slidesToShow: 3,
+            arrows: true,
+            prevArrow: '<div class="arrow-prev arrow"></div>',
+            nextArrow: '<div class="arrow-next arrow"></div>',
+            infinite: false,
+            responsive: [
+                {
+                    breakpoint: 1025,
+                    settings: {
+                        arrows: false,
+                        dots: true
+                    }
+                }
+            ]
+        })
+    } else if ($(window).width() <= '360') {
+        $('.teachers-slider').slick({
+            slidesToShow: 1,
+            arrows: false,
+            dots: true,
+            infinite: false,
+        })
+    } else if ($(window).width() < '768') {
+        $('.teachers-slider').slick({
+            centerMode: true,
+            centerPadding: '85px',
+            variableWidth: true,
+            variableHeight: true,
+            slidesToShow: 3,
+            arrows: false,
+            dots: true,
+            infinite: true,
+        })
+    }
+
+    /**
+     * Кнопка с вопросом
+     */
+    $(".specialities__ask").hover(function () {
+        $(".specialities__ask").removeClass("hidden")
+    }, function () {
+        $(".specialities__ask").addClass("hidden")
+    })
+
+    /**
+     * Определение кнопок в блоке документов
+     */
+    let btnArrows = $(".documents-item__head--btn")
+    let docs = $(".documents-item")
+
+    for (let i = 0; i < btnArrows.length; i++) {
+        btnArrows[i].addEventListener("click", function () {
+            if (docs[i].classList.contains("close"))
+                docs[i].classList.remove("close")
+            else
+                docs[i].classList.add("close")
+        })
+    }
+
+    /**
+     * Показать больше в специальностях
+     */
+    let showMore = $(".specialities__card-more")
+    let cardsMore = $(".specialities__card-with-more")
+
+    for (let i = 0; i < showMore.length; i++) {
+        showMore[i].addEventListener("click", function () {
+            if (cardsMore[i].classList.contains("show"))
+                cardsMore[i].classList.remove("show")
+            else
+                cardsMore[i].classList.add("show")
+        })
+    }
+
+    /**
+     * Форма для заявки
+     */
+    $('.btn-open').on("click", function () {
+        $('.pop-up__card').addClass('active')
+        $('.pop-up__wall').addClass('active')
+    })
+    $('.specialities__ask--text').on("click", function () {
+        $('.pop-up__card').addClass('active')
+        $('.pop-up__wall').addClass('active')
+    })
+
+    $('.pop-up__close-btn').on("click", function () {
+        $('.pop-up__card').removeClass('active')
+        $('.pop-up__wall').removeClass('active')
+    })
+})
